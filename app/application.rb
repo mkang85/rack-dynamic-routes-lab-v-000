df8941aba1af0b7551b60d7ec7559c742d6b36f0
Michael Kang <michaelkang85@gmail.com>
2018-04-19 23:46:20 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+
+
+class Application
+
+  @@item = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+        
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+
+end
