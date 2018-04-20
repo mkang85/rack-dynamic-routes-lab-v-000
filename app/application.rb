e0c316b86121733ef66e49b0fa8ace65dae31208
Michael Kang <michaelkang85@gmail.com>
2018-04-20 01:56:01 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    binding.pry
+    if req.path.match(/items/)
+      item_name = req.path.split("/")[-1]
+      @@items.each do |item|
+        if item.name == item_name
+          resp.write item.price
+          return resp.finish
+        end
+      end
+      resp.write "Item not found"
+      resp.status = 400
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
