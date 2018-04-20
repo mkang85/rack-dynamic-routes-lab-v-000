b6b389a5a856411a6bfe4818719addc4f18fc3b7
Michael Kang <michaelkang85@gmail.com>
2018-04-20 00:09:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,7 @@ class Application
       @@items.each do |item|
         if item.name == item_name
           resp.write item.price
+          return resp.finish
           resp.finish
         end
       end
